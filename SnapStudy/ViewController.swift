afa54118570ddb9643eb5f031dc8fcdd3981c48c
tamaki164 <tamaki164@gmail.com>
2018-01-12 16:13:46 +0900

    Initial Commit

---
+++ SnapStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapStudy
+//
+//  Created by Hiroshi Tamaki on 2018/01/12.
+//  Copyright © 2018年 HiroshiTamaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
